c2badd2e71fe4cc1a2697c1d0a1320a66c001362
Michael Bazett <117475712+Bazinator@users.noreply.github.com>
2026-01-31 19:11:59 -0800

    Delete test.py
    @rohin19 bruh

--- test.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client
-
-url = "https://uvwyfxkxgkwweehpfxfn.supabase.co/"
-key = "sb_secret_J-vPhYSqW9jvCtzPb66Rhw_bvVeGr1h"
-
-supabase = create_client(url, key)
-
-r = supabase.table("places").select("*").execute()
-print(r.data)
